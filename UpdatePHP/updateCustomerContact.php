c5414f70fc4c55deecd5c82f08920e13921b8dc3
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2015-04-10 16:09:12 -0400

    indent fix

--- UpdatePHP/updateCustomerContact.php
+++ UpdatePHP/updateCustomerContact.php
@@ -6,8 +6,8 @@ $CID      = mysqli_real_escape_string($link, $_POST['CID']);
 $cContact = mysqli_real_escape_string($link, $_POST['cContact']);
 
 $sql =  "UPDATE Customers 
-		 SET cContact = '$cContact'
-		 WHERE CID = $CID";   
+	 SET cContact = '$cContact'
+	 WHERE CID = $CID";   
 $result = mysqli_query($link, $sql);
 mysqli_close($link);
 ?>
