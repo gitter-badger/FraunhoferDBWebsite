8e303ffa5b794759fb19d74f0560f09a75cc5975
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2015-04-30 11:31:21 -0400

    now the user is redirected to the selection page if he enters the correct info. Error message if not

--- Login/logincheck.php
+++ Login/logincheck.php
@@ -19,29 +19,28 @@ while($row = mysqli_fetch_array($result)){
 }
 
 if(crypt($password, $hashedPass) == $hashedPass){
-	echo "<div><p>"."User found with this info"."</p></div>";
-
-$_SESSION["username"] = $username;
-
-//This is code to let the user know he is logged in.
-//This is handy while developing but should probably be removed
-$usersql = "SELECT employee_ID, employee_name, employee_email, employee_phone  
-			FROM employee
-			WHERE employee_ID = '$userID'";
-$userResult = mysqli_query($link, $usersql);
-
-while($row = mysqli_fetch_array($userResult)){
-	echo "ID: ".$row[0]."<br>".
-		 "Username: ".$row[1]."<br>".
-		 "Email: ".$row[2]."<br>".
-		 "PhoneNumber: ".$row[3]."<br>";
-}
-echo "Session username is : " . $_SESSION["username"]. "</br>";
-}else{
-	echo "<div><p>"."No results from this query"."</p></div>";
+	echo "success";
+	$_SESSION["username"] = $username;
+	
+	//This is code to let the user know he is logged in.
+	//This is handy while developing but should probably be removed
+	$usersql = "SELECT employee_ID, employee_name, employee_email, employee_phone  
+				FROM employee
+				WHERE employee_ID = '$userID'";
+	$userResult = mysqli_query($link, $usersql);
+	
+	while($row = mysqli_fetch_array($userResult))
+	{
+		echo "ID: ".$row[0]."<br>".
+			 "Username: ".$row[1]."<br>".
+			 "Email: ".$row[2]."<br>".
+			 "PhoneNumber: ".$row[3]."<br>";
+	}
+}else
+{
+	echo "error";
 	session_unset(); 
 	session_destroy(); 
-	die("Wrong info! Are you sure you used your ID instead your full name?");
 }
 
 
