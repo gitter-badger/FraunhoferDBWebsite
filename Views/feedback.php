c2a6faf24db3d63e32320280d25ed425c753254b
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2015-05-11 17:28:04 -0400

    comment page now refreshes if idle so I can see when a new comment comes without refreshing

--- Views/feedback.php
+++ Views/feedback.php
@@ -2,7 +2,24 @@
  <?php
   include '../connection.php';
 ?>
+<script type="text/javascript">
+var idleTime = 0;
+$(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
 
+    //Zero the idle timer on key input.
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+});
+function timerIncrement() {
+    idleTime = idleTime + 1;
+    if (idleTime > 1) { // 2 minutes
+        window.location.reload();
+    }
+}
+</script>  
 <html>
 <head>
 <title>Fraunhofer CCD</title>
